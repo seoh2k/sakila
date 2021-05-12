80bd1024e2b3216d300b9014a4ba77e42acdb31c
seoh2k <seoh2k@gmail.com>
2021-05-12 10:46:42 +0900

    sakila springboot project 초기화 작업

---
+++ sakila/src/main/java/com/gd/sakila/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gd.sakila;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SakilaApplication.class);
+	}
+
+}
---
+++ sakila/src/test/java/com/gd/sakila/SakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
